4812011d71963ec24cf518ec2b1e6cc4397de1a3
Mark Dickson <mark@sitesteaders.com>
2014-01-08 14:29:24 -0500

    added store resource

---
+++ lib/bigcommerce_api/store.rb
@@ -0,0 +1,17 @@
+module BigcommerceAPI  
+
+  class Store < Base
+    attr_accessor :errors, :id, :domain, :name, :address, :phone, :admin_email, :order_email, :language, :currency, :currency_symbol, :currency_symbol_location, :decimal_separator, :thousands_separator, :decimal_places, :weight_units, :dimension_units, :plan_name
+
+    # Sample response
+    # {"id"=>"XXXX", "domain"=>"XXXX.mybigcommerce.com", "name"=>"XXXX", "address"=>"", "phone"=>"", "admin_email"=>"XXX", "order_email"=>"XXX", "language"=>"en", "currency"=>"USD", "currency_symbol"=>"$", "decimal_separator"=>".", "thousands_separator"=>",", "decimal_places"=>2, "currency_symbol_location"=>"left", "weight_units"=>"LBS", "dimension_units"=>"Inches", "plan_name"=>"Partner Sandbox"}
+  	def initialize(data)
+      data.each do |k, v|
+        if v and v.is_a? String
+          val = v.gsub(/\n/, '').gsub(/\t/, '').strip
+        else
+          val = v
+        end
+        send(:"#{k}=", val) if self.respond_to? "#{k}="
+      end
+    end
