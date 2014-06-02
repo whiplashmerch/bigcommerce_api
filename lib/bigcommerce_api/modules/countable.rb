50e45acfae46f111f29982a9a42d46b23f37eafa
Will <machineboy2045@gmail.com>
2014-06-02 15:57:11 -0700

    add filter support to countable

--- lib/bigcommerce_api/modules/countable.rb
+++ lib/bigcommerce_api/modules/countable.rb
@@ -2,8 +2,9 @@ module BigcommerceAPI
 
   module Countable
     
-    def count
-      response = BigcommerceAPI::Base.get("/#{resource}/count")
+    def count(params = {})
+
+      response = BigcommerceAPI::Base.get("/#{resource}/count", :query => date_adjust(params))
       (response.success? and !response.nil?) ? response['count'] : nil
     end
 
