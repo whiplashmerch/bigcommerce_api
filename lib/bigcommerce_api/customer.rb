3e05a844ea2da840ec3199caec7b99e0a82057c2
North <north@tradegecko.com>
2015-01-29 12:55:20 +0800

    Fix customer

--- lib/bigcommerce_api/customer.rb
+++ lib/bigcommerce_api/customer.rb
@@ -1,8 +1,19 @@
 module BigcommerceAPI
   class Customer < Resource
-    attr_accessor :id
+    attr_accessor :id,
+                  :company,
+                  :first_name,
+                  :last_name,
+                  :email,
+                  :phone,
+                  :date_created,
+                  :date_modified,
+                  :store_credit,
+                  :registration_ip_address,
+                  :customer_group_id,
+                  :notes
 
     belongs_to :order
-    has_one :address
+    has_many :addresses
   end
 end
