7dd68f0b7c80522a78b9e29103df389ad5538069
Philip Mark Deazeta <pmdeazeta@gmail.com>
2015-02-23 12:02:05 +0800

    Add custom find query for option set options

--- lib/bigcommerce_api/option_set_option.rb
+++ lib/bigcommerce_api/option_set_option.rb
@@ -18,5 +18,21 @@ module BigcommerceAPI
     def parent
       'option_set'
     end
+
+    def find_for_reload
+      self.class.find(self.option_set_id, self.id)
+    end
+
+    class << self
+      def all(option_set_id, params={})
+        resources = BigcommerceAPI::Base.get("/option_sets/#{option_set_id}/options", query: date_adjust(params))
+        (resources.success? and !resources.nil?) ? resources.collect{|r| self.new(r)} : []
+      end
+
+      def find(option_set_id, id)
+        r = BigcommerceAPI::Base.get("/option_sets/#{option_set_id}/options/#{id}")
+        (r.success? and !r.nil?) ? self.new(r) : nil
+      end
+    end
   end
 end
