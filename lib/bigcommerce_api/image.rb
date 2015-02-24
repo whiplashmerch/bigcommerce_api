ab9d4ca9a57d93c0cf07cdabc9e186e01d666347
Philip Mark Deazeta <pmdeazeta@gmail.com>
2015-02-24 12:35:31 +0800

    Create product image

--- lib/bigcommerce_api/image.rb
+++ lib/bigcommerce_api/image.rb
@@ -13,5 +13,29 @@ module BigcommerceAPI
                   :zoom_url
 
     belongs_to :product
+
+    def resource_url
+      "products/#{self.product_id}/images"
+    end
+
+    def parent
+      'product'
+    end
+
+    def find_for_reload
+      self.class.find(self.product_id, self.id)
+    end
+
+    class << self
+      def all(product_id, params={})
+        resources = BigcommerceAPI::Base.get("/products/#{product_id}/images", query: date_adjust(params))
+        (resources.success? and !resources.nil?) ? resources.collect{|r| self.new(r)} : []
+      end
+
+      def find(product_id, id)
+        r = BigcommerceAPI::Base.get("/products/#{product_id}/images/#{id}")
+        (r.success? and !r.nil?) ? self.new(r) : nil
+      end
+    end
   end
 end
