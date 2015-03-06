b9123b2f76518e859419c4ca8618312eb0f464b9
Philip Mark Deazeta <pmdeazeta@gmail.com>
2015-03-06 16:07:24 +0800

    Fix OptionValue model

--- lib/bigcommerce_api/option_value.rb
+++ lib/bigcommerce_api/option_value.rb
@@ -7,5 +7,29 @@ module BigcommerceAPI
                   :value
 
     belongs_to :option
+
+    def resource_url
+      "options/#{self.option_id}/values"
+    end
+
+    def parent
+      'option'
+    end
+
+    def find_for_reload
+      self.class.find(self.option_id, self.id)
+    end
+
+    class << self
+      def all(option_id, params={})
+        resources = BigcommerceAPI::Base.get("/options/#{option_id}/values", query: date_adjust(params))
+        (resources.success? and !resources.nil?) ? resources.collect{|r| self.new(r)} : []
+      end
+
+      def find(option_id, id)
+        r = BigcommerceAPI::Base.get("/options/#{option_id}/values/#{id}")
+        (r.success? and !r.nil?) ? self.new(r) : nil
+      end
+    end
   end
 end
